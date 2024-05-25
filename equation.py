85a2d9c1443854641833e1404026c0bbf1e6351c
Eric学编程 <1843794003@qq.com>
2024-05-25 19:28:03 +0800

    Update equation.py

--- equation.py
+++ equation.py
@@ -2,7 +2,7 @@ import string
 
 class Base:
     def __init__(self):
-        self.operator=('+','-','*','/','=','>','<','^','.') #目前支持的运算符
+        self.operator=('+','-','*','/','=','>','<','^','.',' ') #目前支持的运算符
 
 class BaseEasyEquation(Base):
     def __init__(self):
@@ -10,15 +10,17 @@ class BaseEasyEquation(Base):
         self.degree=0 #元数
         self.order=0 #次幂数
         self.tool=BaseReturn()
-    def give(self,e):
+    def give(self,e,ignore=()):
         self.eq:str=e
+        self.ignore:tuple=ignore
     def type_(self):
         return self.__class__.__name__
     def syntax_error(self):
         if (
             len(self.tool.amount(self.eq)) != 2
             or self.tool.highest_power(self.eq) != self.order
-            or len(self.tool.unknown(self.eq)) != self.degree
+            or len(self.tool.unknown(self.eq,self.ignore)) != self.degree
+            or len(self.tool.others(self.eq)) > 0
             ):
             return True
         else:
@@ -47,10 +49,10 @@ class BaseReturn(Base):
             return cache
         else:
             return 1
-    def unknown(self,e:str):
+    def unknown(self,e:str,ignore:tuple):
         cache=set()
         for i in range(len(e)):
-            if e[i] in string.ascii_letters:
+            if e[i] in string.ascii_letters and e[i] not in ignore:
                 cache.add(e[i])
         return tuple(cache)
     def others(self,e:str):
@@ -61,7 +63,23 @@ class BaseReturn(Base):
         return tuple(cache)
 
 class OneOne(BaseEasyEquation):
+    def __init__(self):
+        super().__init__()
+        self.degree=1 #元数
+        self.order=1 #次幂数
+
     def get(self,e:str):
         pass
 
-print(OneOne().type_())
+def demo():
+    print(OneOne().type_())
+    a=OneOne()
+    a.give('ax+1')
+    print(a.syntax_error())
+    a.give('ax+1=b',ignore=('a','b'))
+    print(a.syntax_error())
+    a.give('x+2=1')
+    print(a.syntax_error())
+
+if __name__=="__main__":
+    demo()
