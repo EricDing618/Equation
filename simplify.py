7d02ff180f4aa799f53a247defe6ad2bd13af88a
Eric学编程 <1843794003@qq.com>
2024-06-08 13:33:21 +0800

    Update simplify.py

--- simplify.py
+++ simplify.py
@@ -1,53 +1,75 @@
-from base import BaseReturn
-import string
+import re
 
-class Simplify:
-    def __init__(self):
-        self.tool=BaseReturn()
-    def simplify_polynomial_equation(equation):
-        """化简 n 元 n 次方程字符串。
+def simplify_equation_string(equation):
+  """化简n元n次方程字符串。
 
-        例如:
-            simplify_polynomial_equation("x+2=3") == "x=3-2"
-            simplify_polynomial_equation("xy+2xy+1=3") == "3xy=2"
-        """
+  Args:
+    equation: 要化简的方程字符串。
 
-        # 分割等式为左右两部分
-        left, right = equation.split("=")
+  Returns:
+    化简后的方程字符串。
+  """
 
-        # 将左右两部分转换为多项式
-        left_poly = {}
-        right_poly = {}
-        for term in left.split("+"):
-            if term:
-                coeff, var = term.split("*")
-                left_poly[var] = left_poly.get(var, 0) + int(coeff)
-        for term in right.split("+"):
-            if term:
-                coeff, var = term.split("*")
-                right_poly[var] = right_poly.get(var, 0) + int(coeff)
+  # 分离等号两边
+  left, right = equation.split("=")
 
-        # 化简多项式
-        simplified_left = ""
-        simplified_right = ""
-        for var, coeff in left_poly.items():
-            if coeff == 1:
-                simplified_left += var
-            elif coeff == -1:
-                simplified_left += "-" + var
-            else:
-                simplified_left += str(coeff) + var
-        for var, coeff in right_poly.items():
-            if coeff == 1:
-                simplified_right += var
-            elif coeff == -1:
-                simplified_right += "-" + var
-            else:
-                simplified_right += str(coeff) + var
+  # 去除两边的空格
+  left = left.strip()
+  right = right.strip()
 
-        # 返回化简后的等式字符串
-        return simplified_left + "=" + simplified_right
+  # 将等号两边的字符串拆分为项
+  left_terms = re.split(r"(\+|-)", left)
+  right_terms = re.split(r"(\+|-)", right)
 
-if __name__=="__main__":
-    s1=Simplify()
-    print(s1.simplify_polynomial_equation("x+5=6"))
+  # 分别处理等号两边的项
+  simplified_left = simplify_terms(left_terms)
+  simplified_right = simplify_terms(right_terms)
+
+  # 合并化简后的项
+  simplified_equation = f"{simplified_left}={simplified_right}"
+
+  # 返回化简后的方程字符串
+  return simplified_equation
+
+
+def simplify_terms(terms):
+  """化简一个列表中的项。
+
+  Args:
+    terms: 要化简的项列表。
+
+  Returns:
+    化简后的项字符串。
+  """
+
+  # 创建一个字典来存储项和系数
+  term_dict = {}
+
+  # 遍历项
+  for term in terms:
+    # 如果项是一个数字，则将系数添加到常数项中
+    if term.isdigit():
+      term_dict[""] = term_dict.get("", 0) + int(term)
+    # 如果项是一个变量，则将系数添加到变量项中
+    else:
+      term_dict[term] = term_dict.get(term, 0) + 1
+
+  # 将项和系数重新排列成一个字符串
+  simplified_terms = ""
+  for term, coefficient in sorted(term_dict.items()):
+    if coefficient == 0:
+      continue
+    if coefficient == 1:
+      simplified_terms += term
+    else:
+      simplified_terms += f"{coefficient}{term}"
+
+    if simplified_terms and term != "":
+      simplified_terms += " + "
+
+  # 返回化简后的项字符串
+  return simplified_terms.rstrip(" + ")
+
+
+simplified_equation = simplify_equation_string("x+2=3")
+print(simplified_equation)
