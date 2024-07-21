ddb681cd7ad6a8cf5efd9ff683eff9632d1832a3
Eric学编程 <1843794003@qq.com>
2024-07-21 14:48:34 +0800

    v0.1dev0

--- equation.py
+++ equation.py
@@ -5,7 +5,18 @@ class OneOne(BaseEasyEquation):
         super().__init__()
         self.degree=1 #元数
         self.order=1 #次幂数
-
+    def __level2(self):
+        pass
+    def __level3(self):
+        pass
+    def __level4(self):
+        pass
+    def __level5(self):
+        pass
+    def __level6(self):
+        pass
+    def __level7(self):
+        pass
     def make(self): #最后赋值给self.result，不是return
         level=self.tool.level(self.eq,self.ignore)
         difficulty, more_info = level[0],level[1]
@@ -22,6 +33,20 @@ class OneOne(BaseEasyEquation):
                     self.result={self.right:sp.sympify(self.left)}
                 else:
                     raise EquationSyntaxError
+            case 2:
+                self.__level2()
+            case 3:
+                self.__level3()
+            case 4:
+                self.__level4()
+            case 5:
+                self.__level5()
+            case 6:
+                self.__level6()
+            case 7:
+                self.__level7()
+            case _:
+                raise EquationFutureWarning
         
 if __name__=='__main__':
     eq=OneOne()
